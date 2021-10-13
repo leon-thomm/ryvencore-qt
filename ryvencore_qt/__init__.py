edcf9d9161445444a7426b9658643695f7903fe3
Leon Thomm <l.thomm@mailbox.org>
2021-10-13 21:36:36 +0200

    Update __init__.py

--- ryvencore_qt/__init__.py
+++ ryvencore_qt/__init__.py
@@ -7,10 +7,13 @@ Location.PACKAGE_PATH = os.path.normpath(os.path.dirname(__file__))
 # set ryvencore gui mode
 os.environ['RC_MODE'] = 'gui'
 
-# import backend
+# import backend wrapper
 from .src.core_wrapper import *
 
 # import frontend
 from .src.flows.nodes.WidgetBaseClasses import MWB, IWB
 from .src.flows.connections.ConnectionItem import DataConnectionItem, ExecConnectionItem
 from .src.conv_gui import *
+
+# expose ryvencore
+import src.ryvencore as ryvencore
