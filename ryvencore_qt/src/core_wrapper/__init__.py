8ba9aa31bc934e06b04537216105a231b91c4465
leon-thomm <l.thomm@mailbox.org>
2023-02-16 18:14:24 +0100

    remove Node sources

--- ryvencore_qt/src/core_wrapper/__init__.py
+++
@@ -1,9 +0,0 @@
-# unwrapped backend classes
-from ryvencore import InfoMsgs, NodeInputType, NodeOutputType, dtypes, \
-    Flow
-#TODO: include addons here? Need to clarify structure of imports throughout ryvencore-qt, ryvencore and Ryven
-
-# wrapped classes
-from .Node import Node
-from .Session import Session
-# from .WRAPPERS import LogsManager, Logger, VarsManager, Flow, DataConnection
